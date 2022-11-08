0c015a972d1b62e51908408e7bd00fc8d971c8e2
Chris Bryan <chrismbryan@gmail.com>
2022-11-08 11:25:45 +0000

    fixed main.py generation

--- app/src/main.py
+++ app/src/main.py
@@ -13,7 +13,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-
 # pylint: disable=C0103, C0413, E1101
 
 import asyncio
@@ -36,10 +35,7 @@ logging.getLogger().setLevel("DEBUG")
 logger = logging.getLogger(__name__)
 
 
-
-class DeeppurpleApp(VehicleApp):
-
-
+class Dp2App(VehicleApp):
     def __init__(self, vehicle_client: Vehicle):
         super().__init__()
         self.Vehicle = vehicle_client
@@ -47,26 +43,15 @@ class DeeppurpleApp(VehicleApp):
     async def on_start(self):
         print = plugins.Terminal.print
 
-
         logger.info("Reset")
 
         await self.Vehicle.Cabin.Door.Row1.Left.IsOpen.set(False)
 
         await self.Vehicle.Body.Lights.IsBackupOn.set(False)
 
-
-
-            if not isBackupOn:
-                logger.info("Activating driver 1")
-                await self.Vehicle.Cabin.Seat.Row1.Pos1.Height.set(0)
-            else:
-                logger.info("Activation driver 2")
-                await self.Vehicle.Cabin.Seat.Row1.Pos1.Height.set(100)
-
         await self.Vehicle.Body.Lights.IsBackupOn.subscribe(self.onDriverActivation)
 
         await aio.sleep(1)
-
 
         await aio.sleep(3)
 
@@ -83,12 +68,18 @@ class DeeppurpleApp(VehicleApp):
         logger.info("Opening Car Door")
         await self.Vehicle.Cabin.Door.Row1.Left.IsOpen.set(True)
         await self.Vehicle.Cabin.Lights.IsDomeOn.set(True)
+        if not isBackupOn:
+            logger.info("Activating driver 1")
+            await self.Vehicle.Cabin.Seat.Row1.Pos1.Height.set(0)
+        else:
+            logger.info("Activation driver 2")
+            await self.Vehicle.Cabin.Seat.Row1.Pos1.Height.set(100)
+
 
 async def main():
 
-
-    logger.info("Starting DeeppurpleApp...")
-    vehicle_app = DeeppurpleApp(vehicle)
+    logger.info("Starting Dp2App...")
+    vehicle_app = Dp2App(vehicle)
     await vehicle_app.run()
 
 
