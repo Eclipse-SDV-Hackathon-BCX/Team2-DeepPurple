7a7cee7d882a7b41248abffddea525c2d5fb1095
Chris Bryan <chrismbryan@gmail.com>
2022-11-08 11:06:12 +0000

    Update content with digital.auto code

--- app/src/main.py
+++ app/src/main.py
@@ -12,7 +12,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-"""A sample skeleton vehicle app."""
+
 
 # pylint: disable=C0103, C0413, E1101
 
@@ -35,92 +35,60 @@ logging.basicConfig(format=get_opentelemetry_log_format())
 logging.getLogger().setLevel("DEBUG")
 logger = logging.getLogger(__name__)
 
-GET_SPEED_REQUEST_TOPIC = "sampleapp/getSpeed"
-GET_SPEED_RESPONSE_TOPIC = "sampleapp/getSpeed/response"
-DATABROKER_SUBSCRIPTION_TOPIC = "sampleapp/currentSpeed"
 
 
-class SampleApp(VehicleApp):
-    """
-    Sample skeleton vehicle app.
+class DeeppurpleApp(VehicleApp):
 
-    The skeleton subscribes to a getSpeed MQTT topic
-    to listen for incoming requests to get
-    the current vehicle speed and publishes it to
-    a response topic.
-
-    It also subcribes to the VehicleDataBroker
-    directly for updates of the
-    Vehicle.Speed signal and publishes this
-    information via another specific MQTT topic
-    """
 
     def __init__(self, vehicle_client: Vehicle):
-        # SampleApp inherits from VehicleApp.
         super().__init__()
         self.Vehicle = vehicle_client
 
     async def on_start(self):
-        """Run when the vehicle app starts"""
-        # This method will be called by the SDK when the connection to the
-        # Vehicle DataBroker is ready.
-        # Here you can subscribe for the Vehicle Signals update (e.g. Vehicle Speed).
-        await self.Vehicle.Speed.subscribe(self.on_speed_change)
+        print = plugins.Terminal.print
 
-    async def on_speed_change(self, data: DataPointReply):
-        """The on_speed_change callback, this will be executed when receiving a new
-        vehicle signal updates."""
-        # Get the current vehicle speed value from the received DatapointReply.
-        # The DatapointReply containes the values of all subscribed DataPoints of
-        # the same callback.
-        vehicle_speed = data.get(self.Vehicle.Speed).value
 
-        # Do anything with the received value.
-        # Example:
-        # - Publishes current speed to MQTT Topic (i.e. DATABROKER_SUBSCRIPTION_TOPIC).
-        await self.publish_mqtt_event(
-            DATABROKER_SUBSCRIPTION_TOPIC,
-            json.dumps({"speed": vehicle_speed}),
-        )
+        logger.info("Reset")
 
-    @subscribe_topic(GET_SPEED_REQUEST_TOPIC)
-    async def on_get_speed_request_received(self, data: str) -> None:
-        """The subscribe_topic annotation is used to subscribe for incoming
-        PubSub events, e.g. MQTT event for GET_SPEED_REQUEST_TOPIC.
-        """
+        await self.Vehicle.Cabin.Door.Row1.Left.IsOpen.set(False)
 
-        # Use the logger with the preferred log level (e.g. debug, info, error, etc)
-        logger.debug(
-            "PubSub event for the Topic: %s -> is received with the data: %s",
-            GET_SPEED_REQUEST_TOPIC,
-            data,
-        )
+        await self.Vehicle.Body.Lights.IsBackupOn.set(False)
 
-        # Getting current speed from VehicleDataBroker using the DataPoint getter.
-        vehicle_speed = (await self.Vehicle.Speed.get()).value
 
-        # Do anything with the speed value.
-        # Example:
-        # - Publishe the vehicle speed to MQTT topic (i.e. GET_SPEED_RESPONSE_TOPIC).
-        await self.publish_mqtt_event(
-            GET_SPEED_RESPONSE_TOPIC,
-            json.dumps(
-                {
-                    "result": {
-                        "status": 0,
-                        "message": f"""Current Speed = {vehicle_speed}""",
-                    },
-                }
-            ),
-        )
 
+            if not isBackupOn:
+                logger.info("Activating driver 1")
+                await self.Vehicle.Cabin.Seat.Row1.Pos1.Height.set(0)
+            else:
+                logger.info("Activation driver 2")
+                await self.Vehicle.Cabin.Seat.Row1.Pos1.Height.set(100)
+
+        await self.Vehicle.Body.Lights.IsBackupOn.subscribe(self.onDriverActivation)
+
+        await aio.sleep(1)
+
+
+        await aio.sleep(3)
+
+        await self.Vehicle.Cabin.Door.Row1.Left.IsOpen.set(False)
+        await self.Vehicle.Cabin.Lights.IsDomeOn.set(False)
+        await aio.sleep(3)
+
+        await self.Vehicle.Cabin.Door.Row1.Left.IsOpen.set(False)
+
+        logger.info("Finished")
+
+    async def onDriverActivation(self, data: DataPointReply):
+        isBackupOn = data.get(self.Vehicle.Body.Lights.IsBackupOn).value
+        logger.info("Opening Car Door")
+        await self.Vehicle.Cabin.Door.Row1.Left.IsOpen.set(True)
+        await self.Vehicle.Cabin.Lights.IsDomeOn.set(True)
 
 async def main():
 
-    """Main function"""
-    logger.info("Starting SampleApp...")
-    # Constructing SampleApp and running it.
-    vehicle_app = SampleApp(vehicle)
+
+    logger.info("Starting DeeppurpleApp...")
+    vehicle_app = DeeppurpleApp(vehicle)
     await vehicle_app.run()
 
 
